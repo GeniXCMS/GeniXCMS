eaf3992be3b34c7f970a4baf19a7e231a1b3820e
Puguh Wijayanto <hangbowl@gmail.com>
2015-01-31 17:40:53 +0700

    add lib path

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,4 +1,16 @@
 <?php
+
+define('GX_PATH', realpath(__DIR__ . '/../'));
+define('GX_LIB', GX_PATH.'/inc/lib/');
+define('GX_MOD', GX_PATH.'/inc/mod/');
+define('GX_THEME', GX_PATH.'/inc/themes/');
+define('GX_ASSET', GX_PATH.'/assets/');
+
+function __autoload($f) {
+    require GX_LIB. $f . '.class.php';
+}
+
+
 function loader($class)
 {
     $file = $class . '.php';
