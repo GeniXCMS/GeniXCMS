3ecc56f15c321b9cf8ea232e7c107ef3baee48cb
Puguh Wijayanto <hangbowl@gmail.com>
2015-01-31 17:44:00 +0700

    DbTest update

--- Tests/DbTest.php
+++ Tests/DbTest.php
@@ -6,14 +6,12 @@ class DbTest extends PHPUnit_Framework_TestCase
 
     public function setUp()
     {
-        $this->pdo = new PDO($GLOBALS['db_dsn'], $GLOBALS['db_username'], $GLOBALS['db_password']);
-        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $this->pdo->query("CREATE TABLE test_table (what VARCHAR(50) NOT NULL)");
+        Db::query("CREATE TABLE test_table (what VARCHAR(50) NOT NULL)");
     }
 
     public function tearDown()
     {
-        $this->pdo->query("DROP TABLE test_table");
+        Db::query("DROP TABLE test_table");
     }
 
     public function testquery () {
