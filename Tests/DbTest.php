63119b3f9b56489ecfd1e399e3b5bfe96411fad7
Puguh Wijayanto <hangbowl@gmail.com>
2015-02-01 06:55:08 +0700

    DbTest Fix

--- Tests/DbTest.php
+++ Tests/DbTest.php
@@ -8,9 +8,11 @@ class DbTest extends PHPUnit_Framework_TestCase
     {
         //System::config('config');
         define('DB_DRIVER', 'mysqli');
-        define('DBHOST', 'localhost');
-        define('DBUSER', 'root');
-        self::$mysqli = new mysqli(DBHOST, DBUSER, '', 'db_test');
+        define('DB_HOST', 'localhost');
+        define('DB_USER', 'root');
+        define('DB_PASS', '');
+        define('DB_NAME', 'db_test');
+        self::$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
         //mysqli_select_db($link, 'db_test');
         Db::query("CREATE TABLE test_table (what VARCHAR(50) NOT NULL)");
