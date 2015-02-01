be6af88bcfbf3337c4e09150857e0dc5e1571bcb
Puguh Wijayanto <hangbowl@gmail.com>
2015-02-01 23:47:34 +0700

    rss

---
+++ inc/lib/Control/Frontend/rss.control.php
@@ -0,0 +1,18 @@
+<?php if(!defined('GX_LIB')) die("Direct Access Not Allowed!");
+/*
+*    GeniXCMS - Content Management System
+*    ============================================================
+*    Build          : 20140925
+*    Version        : 0.0.1 pre
+*    Developed By   : Puguh Wijayanto (www.metalgenix.com)
+*    License        : MIT License
+*    ------------------------------------------------------------
+* filename : rss.control.php
+* version : 0.0.1 pre
+* build : 20150131
+*/
+
+    Rss::create();
+
+/* End of file rss.control.php */
+/* Location: ./inc/lib/Control/Frontend/rss.control.php */
---
+++ inc/lib/Control/Install/default.control.php
@@ -0,0 +1,15 @@
+<?php if(!defined('GX_LIB')) die("Direct Access Not Allowed!");
+/*
+*    GeniXCMS - Content Management System
+*    ============================================================
+*    Build          : 20140925
+*    Version        : 0.0.1 pre
+*    Developed By   : Puguh Wijayanto (www.metalgenix.com)
+*    License        : MIT License
+*    ------------------------------------------------------------
+* filename : default.control.php
+* version : 0.0.1 pre
+* build : 20150126
+*/
+
+switch 
