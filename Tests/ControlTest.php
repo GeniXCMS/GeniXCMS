827226aea47c6399053ba04227c2274b105b2b54
Puguh Wijayanto <hangbowl@gmail.com>
2015-09-14 01:00:29 +0700

    Commit ControlTest

---
+++ Tests/ControlTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class ControlTest extends PHPUnit_Framework_TestCase 
+{
+
+    public function getControlTest () {
+        $arr = array('ajax', 'post');
+        if(Control::get($arr)){
+            $result = true;
+        }else{
+            $result = false;
+        }
+
+        $this->assertTrue($result);
+
+    }
+
+}
