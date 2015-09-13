882005ba4dda7a967ce47a523980a62c5f126b9d
Puguh Wijayanto <semplon@users.noreply.github.com>
2015-09-13 19:52:21 +0700

    Update CategoriesTest.php

--- Tests/CategoriesTest.php
+++ Tests/CategoriesTest.php
@@ -1,6 +1,21 @@
 class CategoriesTest extends PHPUnit_Framework_TestCase 
 {
     public function addCategoriesTest () {
-        return true;
+        $vars = array(
+            'table' => 'cat',
+            'key' => array(
+                'name' => 'Categories',
+                'parent' => '0',
+                'type' => 'post'
+            )
+        );
+        
+        $db = Db::insert($vars);
+        if($db) {
+            return true;
+        }else{
+            return false;
+        }
+        
     }
 }
