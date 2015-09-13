1ca1806de35fa66dc45996aea3309cdd04418bb3
Puguh Wijayanto <semplon@users.noreply.github.com>
2015-09-14 00:35:31 +0700

    Update CategoriesTest.php

--- Tests/CategoriesTest.php
+++ Tests/CategoriesTest.php
@@ -19,6 +19,13 @@ class CategoriesTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($result);
     }
     
+    public function typeCategoriesTest () {
+        $db = Categories::type(1);
+        $expected = 'post';
+        $this->assertEqual($expected, $db);
+    }
+    
+    
     public function deleteCategoriesTest () {
         $db = Categories::delete(1);
         if($db) {
