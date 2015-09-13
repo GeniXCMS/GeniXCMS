a2d84f81b034bad99ebbcdd03df42142b1cb7853
Puguh Wijayanto <hangbowl@gmail.com>
2015-09-14 01:16:01 +0700

    update test

--- Tests/CategoriesTest.php
+++ Tests/CategoriesTest.php
@@ -1,6 +1,6 @@
 <?php
 
-class CategoriesTest extends \PHPUnit_Framework_TestCase 
+class CategoriesTest extends PHPUnit_Framework_TestCase 
 {
     public function testAddCategories () {
         $vars = array(
