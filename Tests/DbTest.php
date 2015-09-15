1b7f1fb855acd88dd3f4752204e1a3804a8c4669
Puguh Wijayanto <hangbowl@gmail.com>
2015-09-16 02:48:58 +0700

    Tests Update

--- Tests/DbTest.php
+++ Tests/DbTest.php
@@ -5,8 +5,9 @@ class DbTest extends PHPUnit_Framework_TestCase
 
     public function __construct() {
         Db::connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        Db::query("TRUNCATE TABLE IF EXISTS `test_table`");
+        Db::query("DROP TABLE IF EXISTS `test_table`");
         Db::query("CREATE TABLE IF NOT EXISTS `test_table` (what VARCHAR(50) NOT NULL)");
+        Db::query("TRUNCATE TABLE `test_table`");
     }
     
 
