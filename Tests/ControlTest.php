5312a47130585cd37c2c1446df4a012bd7513b04
Puguh Wijayanto <hangbowl@gmail.com>
2015-09-15 20:45:11 +0700

    Tests Update

--- Tests/ControlTest.php
+++ Tests/ControlTest.php
@@ -7,12 +7,8 @@ class ControlTest extends PHPUnit_Framework_TestCase
         $_GET['ajax'] = '';
         $arr = array ('ajax', 'post' ,'page', 'cat', 'mod', 'sitemap', 'rss', 'pay',
             'paidorder', 'cancelorder');
-        if(Control::get($arr)) {
-            $get = true;
-        }else{
-            $get = false;
-        }
-        $this->assertTrue($get);
+        Control::get($arr)
+
     }
 
 }
