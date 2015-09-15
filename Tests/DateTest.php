f3861a17e1d5df6792f50374a4caa95874884aa6
Puguh Wijayanto <hangbowl@gmail.com>
2015-09-16 02:35:43 +0700

    Tests Update

--- Tests/DateTest.php
+++ Tests/DateTest.php
@@ -3,9 +3,23 @@
 class DateTest extends PHPUnit_Framework_TestCase 
 {
 
+    public function __construct () {
+        Db::connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $options = "CREATE TABLE IF NOT EXISTS `options` (
+                    `id` int(11) NOT NULL,
+                      `name` text CHARACTER SET utf8 NOT NULL,
+                      `value` longtext CHARACTER SET utf8 NOT NULL
+                    ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8";
+        Db::query($options);
+        $options = "INSERT INTO `options` (`id`, `name`, `value`) VALUES 
+        (null, 'timezone', 'Asia/Jakarta')";
+        Db::query($options);
+    }
+
     public function testFormat () {
         $date = "2015-09-16 02:29:30";
-        echo Date::format($date);
+        $dformat = Date::format($date);
+        $this->assertEquals("", $dformat)
 
     }
 
