33fc3532d683a9fbce07e73f759b406b92663208
Puguh Wijayanto <hangbowl@gmail.com>
2015-09-15 20:16:14 +0700

    Tests Update

--- Tests/DbTest.php
+++ Tests/DbTest.php
@@ -5,14 +5,8 @@ class DbTest extends PHPUnit_Framework_TestCase
 
     public function __construct() {
         Db::connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        
-    }
-
-    public function testSetUp()
-    {
         Db::query("CREATE TABLE `test_table` (what VARCHAR(50) NOT NULL)");
     }
-
     
 
     public function testQuery () {
