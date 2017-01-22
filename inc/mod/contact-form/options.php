29c0ecd1196517149c36289aa0190d3109fe4dfc
Puguh Wijayanto <psw@metalgenix.com>
2017-01-22 23:19:10 +0700

    Change Contact Form modules options page

--- inc/mod/contact-form/options.php
+++ inc/mod/contact-form/options.php
@@ -1,10 +1,16 @@
 <div class="row">
     <div class="col-md-12">
-        <h1>GeniXCMS Module Sample</h1>
+        <h1><i class="fa fa-envelope"></i> Simple Contact Form</h1>
         <hr />
     </div>
     <div class="col-md-12">
-        This is GeniXCMS Module Sample. Create your own module to make a great applications.
+        <p>
+        This is a Module to <code>+add</code> <strong>Contact Us</strong> page at the Menus manager.
+        You can add new menu and select <kbd>Mod</kbd> options.
+        </p>
+        <p class="alert alert-info">
+            All emails will be sent to your email which is set at the <a href="index.php?page=settings">Settings</a> page.
+        </p>
     </div>
 
 </div>
