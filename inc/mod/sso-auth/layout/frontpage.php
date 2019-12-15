ca1b5f11fdfb02d90225576835ef760bfc06b57c
Puguh Wijayanto <semplon@users.noreply.github.com>
2019-12-15 13:40:24 +0700

    remove unfinished module

--- inc/mod/sso-auth/layout/frontpage.php
+++
@@ -1,8 +0,0 @@
-This is the sample page of the Mod module. You can create your own module page and create the menu link from your module class.
-
-Below is the data passed to the layout file :
-<pre>
-<?php
-    print_r($data);
-?>
-</pre>
